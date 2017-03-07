ba2336b6888957c9ba5b1f97f1caada47de40485
Mark Hintz <markohintz@gmail.com>
2017-03-07 20:02:06 +0100

    Change default sub window render state

--- src/WindowData.h
+++ src/WindowData.h
@@ -29,7 +29,7 @@ public:
 
 	int mId;
 
-	SphereRenderType mSphereRenderType = SphereRenderType::SYPHON_FRAME;
+	SphereRenderType mSphereRenderType = SphereRenderType::TEXTURE;
 
 	ProjectorRef mProjector;
 };
